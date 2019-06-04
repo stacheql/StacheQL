ddee98857c6168324d26ca61649734d05a05532f
Sam Ryoo <samryoo@gmail.com>
2019-06-04 16:59:29 -0700

    Last few steps needed

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ const YELP_API_KEY = process.env.ACCESS_TOKEN;
 
 const config = {
   cacheExpiration: 120, // seconds
+  uniqueVariables: {
+    term: String,
+    location: Number,
+    radius: Number,
+  },
 };
 const stache = new Stache(config);
 
@@ -27,57 +32,7 @@ app.post(
   "/api",
   stache.check,
   (req, res, next) => {
-    // ***SUPERSET ROUTE***
-    if (res.locals.subset && res.locals.offset) {
-      console.log(
-        `*** SUPERSET ROUTE: fetching ${req.body.variables.limit -
-          res.locals.offset} add'l results ***`
-      );
-      req.body.variables.offset = res.locals.offset;
-      req.body.variables.limit = req.body.variables.limit - res.locals.offset;
-      request.post(
-        {
-          url: YELP_API_URL,
-          method: "POST",
-          headers: {
-            Authorization: "Bearer " + YELP_API_KEY,
-          },
-          json: true,
-          body: req.body,
-        },
-        (err, response, body) => {
-          res.locals.superset = Object.assign(
-            {},
-            res.locals.subset,
-            stache.offsetKeys(stache.normalize(body), res.locals.offset)
-          );
-          return next();
-        }
-      );
-    }
-    // ***SUBSET ROUTE***
-    else if (res.locals.subset) {
-      console.log(
-        `*** SUBSET ROUTE: fetching ${
-          req.body.variables.limit
-        } cached results ***`
-      );
-      console.log(`Returned from cache: ${Date.now() - res.locals.start} ms`);
-      res.locals.subset = stache.denormalize(res.locals.subset);
-      res.locals.subset.data.search.total = Date.now() - res.locals.start; // for timer
-      return res.send(res.locals.subset);
-      // ***NO MATCH ROUTE***
-    } else {
-      console.log(
-        `*** NO MATCH: fetching ${req.body.variables.limit} results ***`
-      );
-      req.body.variables.offset = 0; // need to initialize offset for any API request, since it's part of the query in the gql
-      res.locals.clever = true;
-      next();
-    }
-  },
-  (req, res, next) => {
-    if (res.locals.clever) {
+    if (res.locals.httpRequest) {
       request.post(
         {
           url: YELP_API_URL,
@@ -90,7 +45,6 @@ app.post(
         },
         (err, response, body) => {
           res.locals.body = body;
-          console.log("BODY", res.locals.body);
           return next();
         }
       );
