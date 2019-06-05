f9f7a284067043283e0a05ea830bb97002050685
Sam Ryoo <samryoo@gmail.com>
2019-06-05 16:43:37 -0700

    Merging codebase

---
+++ demo/server/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const Stache = require("../stache.js");
+
+const API_URL = "https://api.yelp.com/v3/graphql";
+const API_KEY = process.env.ACCESS_TOKEN;
+
+const config = {
+  cacheExpiration: 120, // seconds
+  uniqueVariables: {
+    term: String,
+    location: Number,
+    radius: Number,
+  },
+  queryObject: "search",
+  queryTypename: "Businesses",
+  flexArg: "limit",
+  offsetArg: "offset",
+};
+const stache = new Stache(config);
+
+app.use(bodyParser.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.post(
+  "/api",
+  stache.check,
+  (req, res, next) => {
+    if (res.locals.httpRequest) {
+      request.post(
+        {
+          url: API_URL,
+          method: "POST",
+          headers: {
+            Authorization: "Bearer " + API_KEY,
+          },
+          json: true,
+          body: req.body,
+        },
+        (err, response, body) => {
+          res.locals.body = body;
+          return next();
+        }
+      );
+    } else {
+      return next();
+    }
+  },
+  stache.it
+);
+
+app.listen(3020);
---
+++ demo/src/components/Search.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="search-page">
+    <search-bar></search-bar>
+    <h1>Load time: <b>{{search.total}} millisecond{{search.total === 1 ? '' : 's'}}</b></h1>
+    <div class="loading" v-if="isLoading">
+      <img src="../assets/timer2.gif"> Loading...
+    </div>
+    <ul v-if="search.business">
+      <li v-for="business in search.business">
+        <br><b>{{business.name}}</b>  {{business.rating}} ☆</br>
+        <br>{{business.location.formatted_address}}</br>
+        <br>{{business.reviews[0].user.name}} says: <i>"{{business.reviews[0].text}}"</i>  </br>
+        <br></br>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import SearchBar from "./SearchBar.vue";
+import gql from "graphql-tag";
+import { _ } from "vue-underscore";
+import Router from "vue-router";
+
+export default {
+  name: "SearchPage",
+  components: {
+    SearchBar
+  },
+  apollo: {
+    $loadingKey: "isLoading",
+    search: {
+      query() {
+        if (this.term && this.location && this.radius && this.limit) {
+          return gql`
+            query searchYelp(
+              $term: String!
+              $location: String!
+              $radius: Float!
+              $limit: Int!
+              $offset: Int!
+            ) {
+              search(
+                term: $term
+                location: $location
+                radius: $radius
+                limit: $limit
+                sort_by: "rating"
+                offset: $offset
+              ) {
+                total
+                business {
+                  name
+                  rating
+                  price
+                  photos
+                  id
+                  location{
+                    formatted_address
+                    city
+                    state
+                  }
+                  reviews{
+                    text
+                    user{
+                      name
+                    }
+                    id
+                    rating
+                    time_created
+                    url
+                  }
+                }
+              }
+            }
+          `;
+        }
+      },
+      variables() {
+        return {
+          term: this.term,
+          location: this.location,
+          radius: this.radius * 1609.34,
+          limit: +this.limit
+        };
+      },
+    }
+  },
+  props: ["term", "location", "radius", "limit"],
+  data() {
+    return {
+      search: {},
+      isLoading: 0,
+    };
+  },
+};
+</script>
+
+<!-- "scoped" attribute limits CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style: none;
+}
+</style>
